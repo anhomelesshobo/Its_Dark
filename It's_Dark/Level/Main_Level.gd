ff8300e37af1e93940ebd5e35d062a62729d1caf
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-11-09 20:57:33 -0500

    player animation and mouvement added/ player added to the game

--- It's_Dark/Level/Main_Level.gd
+++ It's_Dark/Level/Main_Level.gd
@@ -1,27 +1,34 @@
 extends Node2D
 
 var Room = preload("res://Level/Room.tscn")
-onready var Map = $Dungeon
+onready var player = $Player
+onready var Map = $TileMap
 
-var tile_size = 10
-var num_rooms = 50
-var min_size = 4
+
+var tile_size = 16
+var num_rooms = 25
+var min_size = 7
 var max_size = 10
-var hspread = 400
-var vspread = 300
+var hspread = 100
+var vspread = 2000
 var cull = 0.4
 
 
 var path #AStar pathfinding object
+var start_room = null
+var end_room = null
+var play_mode = false
+
 #prim's algorithm
 func _ready():
 	randomize()
 	make_rooms()
+	
 
 # generateur procedural pour les rooms
 func make_rooms():
 	for i in range(num_rooms):
-		var pos = Vector2(rand_range(vspread, hspread), 0)
+		var pos = Vector2(rand_range(vspread, hspread),rand_range(vspread, hspread))
 		var r = Room.instance()
 		var w = min_size + randi() % (max_size - min_size)
 		var h = min_size + randi() % (max_size - min_size)
@@ -35,39 +42,52 @@ func make_rooms():
 		if randf() < cull:
 			room.queue_free()
 		else:
-			room.mode = RigidBody2D.MODE_STATIC
+			room.mode = RigidBody2D.MODE_KINEMATIC
 			room_positions.append(Vector3(room.position.x, room.position.y, 0))
 			
 	yield(get_tree(), 'idle_frame')
 	# generate a minimum spanning tree connecting the rooms
 	path = find_mst(room_positions)
+	make_map()
+	Map.update_bitmask_region()
+
+	player.position = start_room.position
+	play_mode = true
+	$Camera2D/FadeIn.fade_away()
+	
 	
 func _draw():
+	if play_mode:
+		return
 	for room in $Rooms.get_children():
-		draw_rect(Rect2(room.position - room.size, room.size * 2),Color(32,228,0),false)
+		draw_rect(Rect2(room.position - room.size, room.size * 2),
+				 Color(0, 1, 0), false)
 	if path:
 		for p in path.get_points():
 			for c in path.get_point_connections(p):
 				var pp = path.get_point_position(p)
 				var cp = path.get_point_position(c)
-				draw_line(Vector2(pp.x, pp.y), Vector2(cp.x, cp.y), Color(1,1,0), 15, true)
+				draw_line(Vector2(pp.x, pp.y), Vector2(cp.x, cp.y),
+						  Color(1, 1, 0), 15, true)			
 		
 func _process(delta):
 	update()
 
 
-func _input(event):
-	if event.is_action_pressed('ui_select'):
-		for n in $Rooms.get_children():
-			n.queue_free()
-		path = null
-		make_rooms()
-	if event.is_action_pressed('ui_focus_next'):
-		make_map()
+#func _input(event):
+	#if event.is_action_pressed('ui_select'):
+	#	for n in $Rooms.get_children():
+	#		n.queue_free()
+	#	path = null
+	#	make_rooms()
+	#	Map.clear()
+	#if event.is_action_pressed('ui_focus_next'):
+	#	make_map()
+	#	Map.update_bitmask_region()
 
 func find_mst(nodes):
 	# Prim's algorithm
-	var path = AStar.new()
+	path = AStar.new()
 	path.add_point(path.get_available_point_id(), nodes.pop_front())
 	
 	# repeat until no more nodes remain
@@ -84,24 +104,82 @@ func find_mst(nodes):
 					min_dist = p1.distance_to(p2)
 					min_p = p2
 					p = p1
+					
+					
 		var n = path.get_available_point_id()
 		path.add_point(n, min_p)
 		path.connect_points(path.get_closest_point(p), n)
+		
+		
 		nodes.erase(min_p)
 	return path
 
 func make_map():
 	# Create a TileMap from the generated rooms and path
 	Map.clear()
+	find_start_room()
+	find_end_room()
 	
-	#Fill TileMap with walls, then carve empty rooms
-	var full_rect = Rect2()
+	#carve rooms
+	var corridors = [] #One corridor per connection
 	for room in $Rooms.get_children():
-		var r = Rect2(room.position - room.size, room.get_node("CollisionShape2D").shape.extents*2)
-		full_rect = full_rect.merge(r)
-	var topleft = Map.world_to_map(full_rect.position)
-	var bottomright = Map.world_to_map(full_rect.end)
-	for x in range(topleft.x, bottomright.x):
-		for y in range(topleft.y, bottomright.y):
-			Map.set_cell(x,y,1)
+		var s = (room.size / tile_size).floor()
+		var pos = Map.world_to_map(room.position)
+		var ul = (room.position / tile_size).floor() -s
+		for x in range(2,s.x * 2 -1):
+			for y in range(2,s.y * 2 -1):
+				Map.set_cell(ul.x + x, ul.y + y, 3)
+		# Carve connecting corridor
+		var p = path.get_closest_point(Vector3(room.position.x, room.position.y, 0))
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = Map.world_to_map(Vector2(path.get_point_position(p).x, path.get_point_position(p).y))
+				var end = Map.world_to_map(Vector2(path.get_point_position(conn).x, path.get_point_position(conn).y))
+				carve_path(start,end)
+		corridors.append(p)
 		
+func carve_path(pos1, pos2):
+	# Carve a path between two points
+	var x_diff = sign(pos2.x - pos1.x)
+	var y_diff = sign(pos2.y - pos1.y)
+	if x_diff == 0: x_diff = pow(-1.0, randi() % 2)
+	if y_diff == 0: y_diff = pow(-1.0, randi() % 2)
+	# choose either x/y or y/x
+	var x_y = pos1
+	var y_x = pos2
+	if(randi() % 2) > 0:
+		x_y = pos2
+		y_x = pos1
+	for x in range(pos1.x, pos2.x, x_diff):
+		Map.set_cell(x, x_y.y, 3)
+		Map.set_cell(x, x_y.y + y_diff,  3) #widen the corridor
+		Map.set_cell(x, x_y.y + y_diff + -1, 3) #widen the corridor
+		Map.set_cell(x, x_y.y + y_diff + 1, 3) #widen the corridor
+		Map.set_cell(x, x_y.y + y_diff + -2, 3) #widen the corridor
+		Map.set_cell(x, x_y.y + y_diff + 2, 3) #widen the corridor
+	for y in range(pos1.y, pos2.y, y_diff):
+		Map.set_cell(y_x.x, y, 3)	
+		Map.set_cell(y_x.x + x_diff, y, 3) #widen the corridor
+		Map.set_cell(y_x.x + x_diff + -1, y, 3) #widen the corridor
+		Map.set_cell(y_x.x + x_diff + 1, y, 3) #widen the corridor
+		Map.set_cell(y_x.x + x_diff + -2, y, 3) #widen the corridor
+		Map.set_cell(y_x.x + x_diff + 2, y, 3) #widen the corridor
+		
+func find_start_room():
+	var min_x = INF
+	for room in $Rooms.get_children():
+		if room.position.x < min_x:
+			start_room = room
+			min_x = room.position.x
+
+func find_end_room():
+	var max_x = -INF
+	for room in $Rooms.get_children():
+		if room.position.x > max_x:
+			end_room = room
+			max_x = room.position.x
+
+
+func _on_FadeIn_fade_finished():
+	$Camera2D/FadeIn.hide()
+	pass # Replace with function body.
--- It's_Dark/Title_scene/fade_in/fade_in.gd
+++ It's_Dark/Title_scene/fade_in/fade_in.gd
@@ -5,7 +5,8 @@ signal fade_finished
 func fade_in():
 	$AnimationPlayer.play("fade_in")
 	
-
+func fade_away():
+	$AnimationPlayer.play("fade_away")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
